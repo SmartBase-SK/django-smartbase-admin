e70858231ba87f27bd4a3c8d9b9a905aea2c692c
Viliam Mihalik <viliam.mihalik@smartbase.sk>
2024-03-26 16:25:36 +0100

    feat: multiselect checkbox filter and form widget for simple options

---
+++ src/django_smartbase_admin/static/sb_admin/src/js/multiselect.js
@@ -0,0 +1,52 @@
+
+export default class Multiselect {
+    constructor(selector_override, options_override) {
+        const selector = selector_override || '.js-simple-multiselect'
+        this.wrapperSelector = '.js-simple-multiselect-wrapper'
+        document.addEventListener('change', e => {
+            const multiselectInput = e.target.closest(this.wrapperSelector)?.querySelector(selector)
+            if(multiselectInput) {
+                let checked = []
+                this.getCheckboxes(multiselectInput).forEach(el => {
+                    if(el.checked) {
+                        checked.push({
+                            value: el.value,
+                            label: el.dataset.label,
+                        })
+                    }
+                })
+                multiselectInput.value = JSON.stringify(checked)
+                multiselectInput.dispatchEvent(new Event('change'))
+            }
+        })
+        document.querySelectorAll(selector).forEach(el => {
+            this.initMultiselect(el, options_override)
+        })
+    }
+
+    getCheckboxes(base_input) {
+        return base_input.closest(this.wrapperSelector)?.querySelectorAll("input[type='checkbox']")
+    }
+
+    initMultiselect(base_input) {
+        base_input.addEventListener('SBTableFilterFormLoad', () => {
+            if(!base_input.value){
+                return
+            }
+            let checkedCheckboxes = []
+            try {
+                checkedCheckboxes = JSON.parse(base_input.value).map(el => el.value)
+            } catch (e) { /* empty */ }
+            this.getCheckboxes(base_input).forEach(el => {
+                if(checkedCheckboxes.includes(el.value)) {
+                    el.checked = true
+                }
+            })
+
+        })
+        base_input.addEventListener('clear', () => {
+            base_input.closest(this.wrapperSelector)?.querySelectorAll("input[type='checkbox']").forEach(el => el.checked=false)
+            base_input.dispatchEvent(new Event('change'))
+        })
+    }
+}
