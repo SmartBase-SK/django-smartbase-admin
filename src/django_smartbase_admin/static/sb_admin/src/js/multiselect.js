f11b42d9ec3f085cd68784351f8ea63bfbc8fc01
Viliam Mihalik <viliam.mihalik@smartbase.sk>
2024-03-27 10:12:49 +0100

    triv: moved js from html to js

--- src/django_smartbase_admin/static/sb_admin/src/js/multiselect.js
+++ src/django_smartbase_admin/static/sb_admin/src/js/multiselect.js
@@ -1,14 +1,14 @@
-
 export default class Multiselect {
     constructor(selector_override, options_override) {
         const selector = selector_override || '.js-simple-multiselect'
+        const selectorDetail = '.js-simple-multiselect-detail'
         this.wrapperSelector = '.js-simple-multiselect-wrapper'
         document.addEventListener('change', e => {
             const multiselectInput = e.target.closest(this.wrapperSelector)?.querySelector(selector)
-            if(multiselectInput) {
+            if (multiselectInput) {
                 let checked = []
                 this.getCheckboxes(multiselectInput).forEach(el => {
-                    if(el.checked) {
+                    if (el.checked) {
                         checked.push({
                             value: el.value,
                             label: el.dataset.label,
@@ -22,6 +22,14 @@ export default class Multiselect {
         document.querySelectorAll(selector).forEach(el => {
             this.initMultiselect(el, options_override)
         })
+        document.querySelectorAll(selectorDetail).forEach(el => {
+            this.initDetailMultiselect(el)
+        })
+        document.addEventListener('formset:added', (event) => {
+            event.target.querySelectorAll(selectorDetail).forEach(el => {
+                this.initDetailMultiselect(el)
+            })
+        })
     }
 
     getCheckboxes(base_input) {
@@ -30,23 +38,56 @@ export default class Multiselect {
 
     initMultiselect(base_input) {
         base_input.addEventListener('SBTableFilterFormLoad', () => {
-            if(!base_input.value){
+            if (!base_input.value) {
                 return
             }
             let checkedCheckboxes = []
             try {
                 checkedCheckboxes = JSON.parse(base_input.value).map(el => el.value)
-            } catch (e) { /* empty */ }
+            } catch (e) { /* empty */
+            }
             this.getCheckboxes(base_input).forEach(el => {
-                if(checkedCheckboxes.includes(el.value)) {
+                if (checkedCheckboxes.includes(el.value)) {
                     el.checked = true
                 }
             })
 
         })
         base_input.addEventListener('clear', () => {
-            base_input.closest(this.wrapperSelector)?.querySelectorAll("input[type='checkbox']").forEach(el => el.checked=false)
+            base_input.closest(this.wrapperSelector)?.querySelectorAll("input[type='checkbox']").forEach(el => el.checked = false)
             base_input.dispatchEvent(new Event('change'))
         })
+    }
+
+
+    setLabel(wrapper, valueEl) {
+        let labels = []
+        wrapper.querySelectorAll('input[type="checkbox"]').forEach(el => {
+            if (el.checked) {
+                labels.push(document.querySelector(`label[for="${el.id}"]`).innerText)
+            }
+        })
+        valueEl.innerHTML = labels.join(',')
+    }
+
+    clearAll(wrapper, valueEl) {
+        wrapper.querySelectorAll('input[type="checkbox"]').forEach(el => {
+            el.checked = false
+        })
+        this.setLabel(wrapper, valueEl)
+    }
+
+
+    initDetailMultiselect(wrapper) {
+        const valueEl = wrapper.querySelector('.js-value')
+        const clearEl = wrapper.querySelector('.js-clear')
+
+        wrapper.addEventListener('change', () => {
+            this.setLabel(wrapper, valueEl)
+        })
+        clearEl.addEventListener('click', () => {
+            this.clearAll(wrapper, valueEl)
+        })
+        this.setLabel(wrapper, valueEl)
     }
 }
