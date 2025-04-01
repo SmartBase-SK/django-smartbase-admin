db070097ec9d645cd611af08327710c52f0fcc20
LuboZ <33423536+LuboZ@users.noreply.github.com>
2025-04-01 10:46:46 +0200

    triv: #23517 make fuzzy filtering optional - default false (#44)
    * triv: #23517 make fuzzy filtering optional - default false
    
    * triv: #23517 make fuzzy filtering optional - default false
    
    * triv: #23517 make fuzzy filtering optional - default false
    
    * triv: #23517 loading settings in tree_widget.js, updated version
    
    ---------
    
    Co-authored-by: lubos.zlatohlavek <lubos.zlatohlavek@smartbase.sk>

--- src/django_smartbase_admin/admin/widgets.py
+++ src/django_smartbase_admin/admin/widgets.py
@@ -559,7 +559,7 @@ class SBAdminTreeWidget(SBAdminTreeWidgetMixin, SBAdminAutocompleteWidget):
         }
         context["widget"]["additional_columns"] = self.additional_columns
         context["widget"]["tree_strings"] = self.tree_strings
-
+        context["fancytree_filter_settings"] = {}
         return context
 
     @classmethod
