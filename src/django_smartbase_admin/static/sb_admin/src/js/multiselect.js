6bce5226d54c72558d0f4c02bedfbd4ad64b1ef2
Martin Červenka <martin.cervenka@smartbase.sk>
2025-07-03 14:50:34 +0200

    triv: fixed reinitializing of multiselect

--- src/django_smartbase_admin/static/sb_admin/src/js/multiselect.js
+++ src/django_smartbase_admin/static/sb_admin/src/js/multiselect.js
@@ -1,50 +1,52 @@
 export default class Multiselect {
-    constructor(selector_override, options_override) {
+    constructor(selector_override, options_override, target) {
+        target = target || document
         const selector = selector_override || '.js-simple-multiselect'
         const selectorDetail = '.js-simple-multiselect-detail'
         const selectAllClass = 'js-simple-multiselect-all'
         this.wrapperSelector = '.js-simple-multiselect-wrapper'
-        document.addEventListener('change', e => {
-            const wrapperEl = e.target.closest(this.wrapperSelector)
-            const multiselectInput = wrapperEl?.querySelector(selector)
-            const isCheckboxClicked = e.target.type === 'checkbox'
-            const selectAllEl = wrapperEl.querySelector(`.${selectAllClass}`)
-            if (multiselectInput && isCheckboxClicked) {
-                const checkboxes = Array.from(this.getCheckboxes(multiselectInput))
-                if(e.target.classList.contains(selectAllClass)) {
-                    checkboxes.forEach(el => {
-                        el.checked = e.target.checked
-                    })
-                }
-                else {
-                    selectAllEl.checked = false
-                }
-                if(!checkboxes.some(el => el.checked)) {
-                    selectAllEl.checked = true
-                }
-                let checked = []
-                checkboxes.forEach(el => {
-                    if (el.checked) {
-                        checked.push({
-                            value: el.value,
-                            label: el.dataset.label,
+        if(target === document) {
+            document.addEventListener('change', e => {
+                const wrapperEl = e.target.closest(this.wrapperSelector)
+                const multiselectInput = wrapperEl?.querySelector(selector)
+                const isCheckboxClicked = e.target.type === 'checkbox'
+                const selectAllEl = wrapperEl.querySelector(`.${selectAllClass}`)
+                if(multiselectInput && isCheckboxClicked) {
+                    const checkboxes = Array.from(this.getCheckboxes(multiselectInput))
+                    if(e.target.classList.contains(selectAllClass)) {
+                        checkboxes.forEach(el => {
+                            el.checked = e.target.checked
                         })
+                    } else {
+                        selectAllEl.checked = false
                     }
+                    if(!checkboxes.some(el => el.checked)) {
+                        selectAllEl.checked = true
+                    }
+                    let checked = []
+                    checkboxes.forEach(el => {
+                        if(el.checked) {
+                            checked.push({
+                                value: el.value,
+                                label: el.dataset.label,
+                            })
+                        }
+                    })
+                    multiselectInput.value = JSON.stringify(checked)
+                    multiselectInput.dispatchEvent(new Event('change', {bubbles: true}))
+                }
+            })
+            document.addEventListener('formset:added', (event) => {
+                event.target.querySelectorAll(selectorDetail).forEach(el => {
+                    this.initDetailMultiselect(el)
                 })
-                multiselectInput.value = JSON.stringify(checked)
-                multiselectInput.dispatchEvent(new Event('change', {bubbles: true}))
-            }
-        })
-        document.querySelectorAll(selector).forEach(el => {
+            })
+        }
+        target.querySelectorAll(selector).forEach(el => {
             this.initMultiselect(el, options_override)
         })
-        document.querySelectorAll(selectorDetail).forEach(el => {
+        target.querySelectorAll(selectorDetail).forEach(el => {
             this.initDetailMultiselect(el)
-        })
-        document.addEventListener('formset:added', (event) => {
-            event.target.querySelectorAll(selectorDetail).forEach(el => {
-                this.initDetailMultiselect(el)
-            })
         })
     }
 
