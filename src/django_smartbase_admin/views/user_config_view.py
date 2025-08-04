de6595918dbc7027bf77a0678bf90535f57da2d6
Martin Červenka <martin.cervenka@smartbase.sk>
2025-08-04 10:15:36 +0200

    re-added formatted color scheme choices

--- src/django_smartbase_admin/views/user_config_view.py
+++ src/django_smartbase_admin/views/user_config_view.py
@@ -1,18 +1,25 @@
 from django import forms
 from django.http import HttpResponse
+from django.utils.html import format_html
 from django.views.generic import FormView
 
 from django_smartbase_admin.admin.admin_base import SBAdminBaseFormInit
 from django_smartbase_admin.admin.widgets import (
     SBAdminRadioDropdownWidget,
 )
-from django_smartbase_admin.models import SBAdminUserConfiguration
+from django_smartbase_admin.models import SBAdminUserConfiguration, ColorScheme
 from django_smartbase_admin.services.configuration import (
     SBAdminUserConfigurationService,
 )
 
 
 class ColorSchemeForm(SBAdminBaseFormInit, forms.ModelForm):
+    color_scheme_icons = {
+        ColorScheme.AUTO.value: "Translation",
+        ColorScheme.DARK.value: "Moon",
+        ColorScheme.LIGHT.value: "Sun-one",
+    }
+
     class Meta:
         model = SBAdminUserConfiguration
         fields = ("color_scheme",)
@@ -20,6 +27,16 @@ class ColorSchemeForm(SBAdminBaseFormInit, forms.ModelForm):
             "color_scheme": SBAdminRadioDropdownWidget(),
         }
         required = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices_formatted = []
+        for choice in self.fields["color_scheme"].choices:
+            choice_label = format_html(
+                f'<span class="flex gap-8"><svg class="w-20 h-20"><use href="#{self.color_scheme_icons.get(choice[0])}"></use></svg><span>{choice[1]}</span></span>'
+            )
+            choices_formatted.append((choice[0], choice_label))
+        self.fields["color_scheme"].choices = choices_formatted
 
 
 class ColorSchemeView(FormView):
