b67f3aee8ebe3d3cecf70ad3b253a42e98711706
Martin Červenka <martin.cervenka@smartbase.sk>
2026-04-15 10:36:01 +0200

    fix sb_admin_site import

--- src/django_smartbase_admin/apps.py
+++ src/django_smartbase_admin/apps.py
@@ -2,8 +2,6 @@ from django.apps import AppConfig
 from django.utils.module_loading import autodiscover_modules
 from django.conf import settings
 
-from django_smartbase_admin.admin.site import sb_admin_site
-
 
 class SBAdminConfig(AppConfig):
     name = "django_smartbase_admin"
@@ -14,6 +12,7 @@ class SBAdminConfig(AppConfig):
         super().ready()
         from .monkeypatch import fake_inline_monkeypatch
         from .monkeypatch import admin_readonly_field_monkeypatch
+        from django_smartbase_admin.admin.site import sb_admin_site
 
         if settings.SB_ADMIN_CONFIGURATION:
             autodiscover_modules("sb_admin", register_to=sb_admin_site)
